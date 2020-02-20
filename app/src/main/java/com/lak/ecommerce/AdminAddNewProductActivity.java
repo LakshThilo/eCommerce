ebbf2e8ec3440fdf3541921712111be2a30e802a
Lakshitha <lakshthilo9094@gmail.com>
2020-02-20 00:17:04 +0000

    Adding components and implementing AdminAddNewProduct class

--- app/src/main/java/com/lak/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/lak/ecommerce/AdminAddNewProductActivity.java
@@ -3,11 +3,17 @@ package com.lak.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    private ImageView imageInputProduct;
+    private Button btnAddNewProduct;
+    private EditText inputProductName, inputProductDescription, inputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,13 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         categoryName = getIntent().getExtras().get("category").toString();
 
         Toast.makeText(this,categoryName,Toast.LENGTH_SHORT).show();
+
+        imageInputProduct = findViewById(R.id.select_product_image);
+        btnAddNewProduct = findViewById(R.id.add_new_product);
+        inputProductName = findViewById(R.id.product_name);
+        inputProductDescription = findViewById(R.id.product_description);
+        inputProductPrice = findViewById(R.id.product_price);
+
 
 
     }
